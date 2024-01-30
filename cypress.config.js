b7a98639071501779b449c589c704239088add54
Steffi Joji <steffijoji@gmail.com>
2024-01-30 08:29:22 -0300

    add screen size

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://github.com/",
     defaultCommandTimeout: 5000,
+    viewportWidth: 1440,
+    viewportHeight: 900,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
