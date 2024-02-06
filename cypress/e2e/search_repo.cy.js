9d94f8cc4a2a4ac4464c0134745d7b00007145c9
Steffi Joji <steffijoji@gmail.com>
2024-02-06 09:22:34 -0300

    search for repositories

---
+++ cypress/e2e/search_repo.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+import home_page from "../support/pages/home_page"
+import results_page from "../support/pages/results_page"
+import user_page from "../support/pages/user_page"
+
+describe('Search', () => {
+
+  beforeEach('Access GitHub page', () => {
+    home_page.accessHomePage()
+  })
+
+  it('Search for a user on GitHub', () => {
+    home_page.searchUser('steffijoji{enter}')
+  })
+
+  it('Find search results', () => {
+    home_page.searchUser('steffijoji{enter}')
+    results_page.accessResultsPage()
+  })
+
+  it('Finds user in results', () => {
+    home_page.searchUser('steffijoji{enter}')
+    results_page.accessResultsPage()
+    results_page.findUser()
+  })
+
+  it('Access user page', () => {
+    home_page.searchUser('steffijoji{enter}')
+    results_page.accessResultsPage()
+    results_page.findUser()
+    user_page.accessUserPage()
+  })
+
+  it.only('Access repositories', () => {
+    home_page.searchUser('steffijoji{enter}')
+    results_page.accessResultsPage()
+    results_page.findUser()
+    user_page.accessUserPage()
+    user_page.accessRepositories()
+  })
+
+})
--- cypress/support/pages/user_page.js
+++ cypress/support/pages/user_page.js
@@ -4,5 +4,10 @@ export default {
     accessUserPage() {
         cy.get('.p-nickname.vcard-username.d-block')
             .should('contain.text', 'steffijoji')
+    },
+
+    accessRepositories() {
+        cy.get('main > div.mt-4 * > a:nth-child(2)')
+            .click()
     }
 }
